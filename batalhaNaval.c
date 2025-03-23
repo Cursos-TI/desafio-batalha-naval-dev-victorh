6f880ee508b2399e8315af5300dbac82f6b055a5
dev-victorh <dev_victor@outlook.com>
2025-03-23 23:32:55 +0000

    Atualização do código para o nível aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,15 +1,53 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    int Tabuleiro[10][10];
 
+    for (int i = 0; i < 10; i++) {   // Inicializando tabuleiro com 0
+        for (int j = 0; j < 10; j++) {
+            Tabuleiro[i][j] = 0;
+        }
+    }
+
+    for (int j=1; j<4; j++){ // atribuindo posição do navio horizontal, de B3 A D3
+        if (j<0 || j>9){ // validação de posição válida
+            printf("Posição inválida\n");
+            break;
+        }
+        Tabuleiro[2][j] = 3;
+    }
+    
+    for (int i=6; i<9; i++){ // atribuindo posição do navio vertical, de I7 a I9
+        if (i<0 || i>9){ // validação de posição válida
+            printf("Posição inválida\n");
+            break;
+        }
+        if (Tabuleiro[i][8] == 3){ // validação de sobreposição de navios simplificada. Msg de erro será exibida e tabuleiro será imprimido com apenas o primeiro navio
+            printf("Navios sobrepostos!\n");
+            break;
+        }
+        Tabuleiro[i][8] = 3;
+    }
+
+    printf("   |"); // imprimindo espaço e demarcador da matriz
+    for (int i = 65; i<75; i++) { // utilizando a tabela ASCII para imprimir as letras
+        printf(" %c |", i); //impressão do índice da coluna, utilizando a tabela ASCII
+    }
+
+    printf("\n");
+
+    for (int i = 0; i < 10; i++) { // loop aninhado para impressão do tabuleiro
+        if (i<=8){ // condicional para imprimir o espaço antes do número, caso a numeração seja menor que 10 (posição 0 a 8 - 9 posições)
+            printf(" %d |", i+1); //impressão do índice da linha
+        }else{ // caso seja maior que 10, não imprime o espaço
+            printf("%d |", i+1); // impressão do índice da linha, caso numeração >=10, ou posição no vetor >=9
+        }
+        for (int j = 0; j < 10; j++) {
+            printf(" %d |", Tabuleiro[i][j]); //impressão do tabuleiro
+        }
+        printf("\n");
+    }
+    
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
